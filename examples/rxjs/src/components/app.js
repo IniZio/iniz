17f44dbef1a040b7639bf29659edc2b40a2de3db
inizio <digit4free@gmail.com>
2018-08-12 11:53:52 +0800

    doc(rxjs): counter example without reim

--- examples/rxjs/src/components/app.js
+++ examples/rxjs/src/components/app.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import {componentFromStream} from 'recompose'
-import { interval } from 'rxjs';
-import {map} from 'rxjs/operators'
+import {createEventHandler, mapPropsStream, compose} from 'recompose'
+import {merge} from 'rxjs';
+import {map, switchMap, scan, startWith, mapTo} from 'rxjs/operators'
 
-const App = componentFromStream(props$ =>
-  props$.pipe(
-    map(({count}) => (
-      <div>{count}</div>
-    ))
-  )
+const App = ({count, increment, decrement}) => (
+  <div>
+    <div>Count: {count}</div>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+  </div>
 )
 
-export default App
+const counter = mapPropsStream(props$ => {
+  const { handler: increment, stream: increment$ } = createEventHandler()
+  const { handler: decrement, stream: decrement$ } = createEventHandler()
+
+  return props$.pipe(
+      switchMap(props =>
+        merge(
+          increment$.pipe(mapTo(1)),
+          decrement$.pipe(mapTo(-1))
+        )
+          .pipe(
+            startWith(props.count),
+            scan((count, n) => count + n, 0),
+            map(count => ({...props, count, increment, decrement}))
+          )
+      )
+    )
+})
+
+export default compose(
+  counter
+)(App)
