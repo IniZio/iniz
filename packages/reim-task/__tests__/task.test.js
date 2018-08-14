1daaafb1a2629a133036c28650a30134a825306f
IniZio <newman.kcchow@gmail.com>
2018-08-14 21:51:21 +0800

    feat(task): adds task function (#14)
    * feat(task): adds task function
    
    * style: fixes lint errors
    
    * ci: build should be before test

---
+++ packages/reim-task/__tests__/task.test.js
@@ -0,0 +1,30 @@
+import {task} from '../src'
+
+test('triggers hook throughout the process', async () => {
+  const subscriber1 = jest.fn()
+  const subscriber2 = jest.fn()
+
+  const testTask = task(() => {
+    return 'resolved task'
+  }, subscriber1)
+
+  testTask.subscribe(subscriber2)
+
+  await testTask()
+
+  expect(subscriber1).toBeCalledWith({status: 'pending'})
+  expect(subscriber1).toBeCalledWith({status: 'resolved', result: 'resolved task'})
+
+  expect(subscriber2).toHaveBeenCalledTimes(2)
+
+  const subscriber3 = jest.fn()
+  const failTask = task(() => {
+    throw new Error('woops')
+  }, subscriber3)
+  try {
+    await failTask()
+  } catch (e) {
+    // Once for pending, once for rejected
+    expect(subscriber3).toHaveBeenCalledTimes(2)
+  }
+})
