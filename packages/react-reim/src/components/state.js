ca9f22984000a0d850ea8394b92697f67526a063
inizio <newman.kcchow@gmail.com>
2018-12-22 14:39:01 +0800

    fix(react): non-array setter

--- packages/react-reim/src/components/state.js
+++ packages/react-reim/src/components/state.js
@@ -56,7 +56,7 @@ class State extends Component {
   }
 
   setSetter() {
-    const setters = this.props.setter.reduce((acc, val) => ({...acc, ...(typeof val === 'function' ? val(this.store) : val)}), {})
+    const setters = [].concat(this.props.setter).reduce((acc, val) => ({...acc, ...(typeof val === 'function' ? val(this.store) : val)}), {})
 
     const cache = Object.keys(setters)
       .reduce((acc, k) => ({...acc, [k]: (...args) => {
