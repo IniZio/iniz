aa879eb01fe494984b60dcaa6d230b567debdb93
inizio <newman.kcchow@gmail.com>
2022-10-30 09:55:06 +0800

    Proxy ownKeys to track key length in object

--- packages/core/src/computed.test.ts
+++ packages/core/src/computed.test.ts
@@ -33,6 +33,19 @@ describe("computed", () => {
     expect(c1()).toBe("xyz 10");
   });
 
+  it("should be able to track object key update", () => {
+    const errors = atom({});
+    const errorCount = computed(() => Object.keys(errors()).length);
+
+    expect(errorCount()).toBe(0);
+    errors({ age: { min: 10, actual: 0 } });
+    expect(errorCount()).toBe(1);
+    errors({});
+    expect(errorCount()).toBe(0);
+    errors({ age: { min: 10, actual: 0 }, email: { suffix: "bcd.com" } });
+    expect(errorCount()).toBe(2);
+  });
+
   // TODO: I am actually not sure how to fit this logic in yet lol
   it.todo("should forbidden updating", () => {
     const a1 = atom("abc");
