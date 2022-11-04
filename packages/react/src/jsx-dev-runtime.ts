25343e698f947eb064d2f17406685addf79a8114
inizio <newman.kcchow@gmail.com>
2022-11-04 09:20:53 +0800

    Export fragment from jsx runtime

--- packages/react/src/jsx-dev-runtime.ts
+++ packages/react/src/jsx-dev-runtime.ts
@@ -2,4 +2,7 @@
 import { jsxDEV as ReactJsxDEV } from "react/jsx-dev-runtime";
 import { proxyCreateElement } from "./createElement";
 
+// @ts-ignore
+export { Fragment } from "react/jsx-runtime";
+
 export const jsxDEV = proxyCreateElement(ReactJsxDEV);
--- packages/react/src/jsx-runtime.ts
+++ packages/react/src/jsx-runtime.ts
@@ -2,5 +2,8 @@
 import { jsx as ReactJsx, jsxs as ReactJsxs } from "react/jsx-runtime";
 import { proxyCreateElement } from "./createElement";
 
+// @ts-ignore
+export { Fragment } from "react/jsx-runtime";
+
 export const jsx = proxyCreateElement(ReactJsx);
 export const jsxs = proxyCreateElement(ReactJsxs);
